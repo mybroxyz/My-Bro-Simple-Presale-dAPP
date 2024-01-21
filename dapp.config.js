71252455b66ee11d13bf7866202033c2337b3ab1
breadNbutter42 <mountainsunrise79@gmail.com>
2024-01-21 16:43:58 -0500

    removed wl check

--- dapp.config.js
+++ dapp.config.js
@@ -1,7 +1,7 @@
 const config = {
   title: 'Weekend Wankers',
   description: 'WANKERS',
-  contractAddress: '0x2571aEafC248cd79dA50af17b2Ef9E45912Ed027',
+  contractAddress: '0xWaNkEr',
   maxMintAmount: 10,
   presaleMaxMintAmount: 1,
   price: 1

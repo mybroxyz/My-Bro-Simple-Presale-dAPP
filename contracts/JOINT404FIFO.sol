13f927f883bedf2d91e57214d071e8bca0e8002c
breadNbutter42 <mountainsunrise79@gmail.com>
2024-04-22 17:09:05 -0400

    JOINT404-FIFO

--- contracts/JOINT404FIFO.sol
+++ contracts/JOINT404FIFO.sol
@@ -1,10 +1,14 @@
 //JOINT404-FIFO converts a user's specific NFTs to ERC20 tokens, with a FIFO array method for converting back to ERC721.
-//Written by @0xJelle for @ChiknOGMeme to wrap their $WEED NFT with 404 functionality.
+//Created by @0xJelle for @ChiknOGMeme to wrap $WEED.
 
-//Import file: utils/ReentrancyGuard.sol
+
+
+//File import: utils/ReentrancyGuard.sol
 
 // License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (utils/ReentrancyGuard.sol)
+
+//pragma solidity ^0.8.20;
 
 /**
  * @dev Contract module that helps prevent reentrant calls to a function.
@@ -91,7 +95,6 @@ abstract contract ReentrancyGuard {
 
 
 
-
 interface IERC20 {
     function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
@@ -120,101 +123,150 @@ interface IERC721 {
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.24;
 
-contract JOINT404 is ReentrancyGuard {
 
-    address constant ERC20_ADDRESS = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7; //token address
-    address constant ERC721_ADDRESS = 0xb92A5f94466305659976e95c98131B86D8982375; //NFT address
+contract JOINT404 {
 
-    uint constant TOTAL_SUPPLY = 420; //NFT total supply
-    uint constant TOKENS_PER_NFT_GWEI = 1000000000000000000000000; //A million tokens in GWEI per NFT
-    uint constant MAX_NFT_WITHDRAWAL = 10; //Max per transaction
+    address constant ERC20_ADDRESS = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7; //token address.
+    address constant ERC721_ADDRESS = 0xb92A5f94466305659976e95c98131B86D8982375; //NFT address.
+
+    uint256 constant TOKENS_PER_NFT_GWEI = 1000000 ETH; //1 Million whole tokens per NFT.
+    uint256 constant TOTAL_SUPPLY = 420; //NFT total supply.
+    uint256 constant MAX_NFT_TRANSFERS = 200; //Max per transaction to prevent out of gas errors.
+
+    IERC20 constant ITOKEN = IERC20(ERC20_ADDRESS); //ERC20 token contract interface.
+    IERC721 constant INFT = IERC721(ERC721_ADDRESS); //ERC721 NFT contract interface. 
+
+    
+    uint256 public depositIndex; //Index to track where to overwrite in the array for FIFO when depositing NFTs.
+    uint256 public withdrawIndex; //Index to track where to read from in the array for FIFO when withdrawing NFTs.
+    uint16[](TOTAL_SUPPLY) public nftArray; //Array of NFT IDs deposited into contract, can change uint16 size as needed.
 
 
-    IERC20 token = IERC20(ERC20_ADDRESS); //ERC20 token contract interface
-    IERC721 nft = IERC721(ERC721_ADDRESS); //ERC721 NFT contract interface
+    event TokensDeposited(address indexed user, uint amount); //To see the NFT IDs withdrawn to user from here check the NFTs contract transfer event logs or add to event here.
+    event NFTsDeposited(address indexed user, uint[] tokenIds); //To see the amount of tokens withdrawn to user check the ERC20 contract transfer event logs or add to event here.
 
-    uint[] public nftArray; //Array of NFTs deposited into contract
+    constructor() {
+        require(ERC20_ADDRESS != address(0), "ERC20 address must not be 0");
+        require(ERC721_ADDRESS != address(0), "ERC721 address must not be 0");
+        require(TOKENS_PER_NFT_GWEI > 0, "Tokens per NFT must be greater than 0");
+        require(TOTAL_SUPPLY > 0, "Total supply must be greater than 0");
+        require(TOTAL_SUPPLY < 65536, "Update nftArray to use uint32 if total supply exceeds 65536");
+        require(TOTAL_SUPPLY > 256, "Update nftArray to use uint8 if total supply is less than 257"); 
+        require(MAX_NFT_TRANSFERS > 0, "Max NFT withdrawal must be greater than 0");
+        require(MAX_NFT_TRANSFERS <= 200, "Max NFT withdrawal must be less than 200"); //Test and see what the max is for your particular setup to prevent out of gas errors.    
+    }
 
-    event TokensDeposited(address indexed user, uint amount);
-    event NFTsDeposited(address indexed user, uint[] tokenIds);
 
+        //Public functions:
 
-    /// @dev Deposits ERC20 tokens into the contract and adds to nftsOwed mapping for the user.
-    /// @param amount_ The amount of ERC20 tokens to deposit in GWEI.
+        /// @dev Deposits ERC20 tokens into the contract and sends ERC721 tokens to the user in exchange.
+        /// @param amount_ The amount of ERC20 tokens to deposit in GWEI.
     function depositERC20(uint amount_) public nonReentrant {
-        require((amount_ % TOKENS_PER_NFT_GWEI) == 0, "Amount must be in whole unit values");
-        uint nftAmount_ = amount_ / TOKENS_PER_NFT_GWEI; //Total number of NFTs to send to user immediately
-        require(nftAmount_ <= MAX_NFT_WITHDRAWAL, "Amount exceeds max NFT withdrawal limit");
-        require(nftsOwed_ > 0, "Amount must be greater than 0");
+        require(amount_ > 0, "Amount must be greater than 0");
+        require((amount_ % TOKENS_PER_NFT_GWEI) == 0, "Amount deposited must be in whole unit values of 1 Million tokens per NFT");
+        uint256 nftAmount_ = amount_ / TOKENS_PER_NFT_GWEI; //Total number of NFTs to send to user immediately.
+        require(nftAmount_ <= MAX_NFT_TRANSFERS, "Amount exceeds max NFT withdrawal limit of 200 NFTs per transaction");
+        //require(nftAmount <= nftArray.length, "Not enough NFTs in the array to withdraw amount requested"); //If extra ERC20 circulating other than seeded into this contract.
         tokenTransfer(msg.sender, address(this), amount_);
-        recentRequestBlock[msg.sender] = block.number + 3;
-        nftsOwed[msg.sender] += nftAmount_;
+        withdrawNFTs(nftAmount_);
         emit TokensDeposited(msg.sender, amount_);
     }
 
 
-    /// @dev Withdraws owed ERC721 tokens from the contract to the user.
-    function withdrawNFTs() public nonReentrant {
-        address msgSender_ = msg.sender;
-        uint nftsOwed_ = nftsOwed[msgSender_];
-        require(block.number >= recentRequestBlock[msgSender_], "User is too early, must wait 3 blocks from deposit");
-
-        uint counter_ = nftsOwed_;
-        if (nftsOwed_ > MAX_NFT_WITHDRAWAL) {
-            counter_ = MAX_NFT_WITHDRAWAL;
-        }
-        nftsOwed[msgSender_] -= counter_;
-
-        for (uint i = 0; i < counter_; i++) {
-            uint pickedIndex_ = ((uint(keccak256(abi.encodePacked(block.prevrandao(recentRequestBlock[msg.sender]), i, uint(msg.sender), msg.nonce))) % nftArray.length) - 1;
-            uint tokenId_ = nftArray[pickedIndex_];
-
-            nftTransfer(address(this), msg.sender, tokenId_);
-        }
-
-        emit NFTsWithdrawn(msgSender_, counter_);
-    }
-
-    /// @dev Deposits an array of ERC721 tokens into the contract and instantly sends ERC20s to the user in exchange.
-    /// @param idArray_ The array of ERC721 token IDs to deposit.
-    function depositERC721ArrayAndWithdrawERC20(uint[] memory idArray_) public nonReentrant {
-        uint length_ = idArray_.length;
+        /// @dev Deposits an array of ERC721 tokens into the contract and instantly sends ERC20s to the user in exchange.
+        /// @param idArray_ The array of ERC721 token IDs to deposit.
+    function depositERC721Array(uint16[] memory idArray_) public nonReentrant {
+        uint256 length_ = idArray_.length;
         require(length_ > 0, "ID Array must not be empty");
+        require(length_ <= MAX_NFT_TRANSFERS, "Amount exceeds max NFT deposit limit of 200 NFTs per transaction");
+        address addressThis_ = address(this);
+        //require(length_ * TOKENS_PER_NFT_GWEI <= ITOKEN.balanceOf(addressThis_), "Not enough tokens available now"); //If less ERC20 seeded into contract than circulating NFT supply.
+        uint256 depositIndex_ = depositIndex;
+        uint256 msgSender_ = msg.sender;
+        uint256[] storage nftArray_ = nftArray; // Storage pointer to save gas on the multiple writes in the following loop.
 
-        for (uint i = 0; i < length_; i++) {
-            nftTransfer(msg.sender, address(this), idArray_[i]);
-            nftArray.push(idArray_[i]);
+        for (uint256 i = 0; i < length_; ++i) {
+            nftTransfer(msgSender_, addressThis_, idArray_[i]);
+            nftArray_[depositIndex_] = idArray_[i];
+            ++depositIndex_;
+            if (depositIndex_ == TOTAL_SUPPLY) { //Reset index to start if it reaches the end of the array.
+                depositIndex_ = 0;
+            }
         }
 
+        depositIndex = depositIndex_;
+        uint256 amount_ = length_ * TOKENS_PER_NFT_GWEI; //Total number of tokens in GWEI to send to user immediately.
+        tokenTransfer(address(this), msg.sender, amount_);
         emit NFTsDeposited(msg.sender, idArray_);
     }
 
-    function tokenTransfer(address sender_, address receiver_, uint amount_) private returns (bool) {
-        return token.transferFrom(sender_, receiver_, amount_);
-    }
 
-    function nftTransfer(address sender_, address receiver_, uint tokenId_) private {
-        nft.transferFrom(sender_, receiver_, tokenId_);
-    }
+    //Private functions:
 
-    fallback() payable nonReentrant {
-        uint amountSentGwei_ = msg.value;
-        require(amountSentGwei_ <= GWEI_MAX + 1, "Wrong amount of coded gwei sent");
-        if (amountSentGwei_ >= SIGNIFIER_DIGIT) {
-            uint[] memory arrayID;
-            arrayID.push(amountSentGwei_ - SIGNIFIER_DIGIT);
-            depositERC721ArrayAndWithdrawERC20(arrayID_);
-        } else if (amountSentGwei_ == TOTAL_SUPPLY + 1) {
-            withdrawNFTs();
-        } else if (amountSentGwei_ <= TOTAL_SUPPLY) {
-            tokenTransfer(msg.sender, address(this), amountSentGwei_);
+        /// @dev Withdraws ERC721 tokens from the contract to the user in FIFO order.
+        /// @param nftsOwed_ The number of NFTs owed to the user.
+    function withdrawNFTs(uint256 nftsOwed_) private {
+        uint256 withdrawIndex_ = withdrawIndex;
+        uint256 msgSender_ = msg.sender;
+        uint256[] storage nftArray_ = nftArray; // Storage pointer to save gas on the multiple reads in the following loop.
+
+        for (uint256 i = 0; i < nftsOwed; ++i) {
+            nftTransfer(address(this), msgSender_, nftArray_[withdrawIndex_]);
+            ++withdrawIndex_;
+            if (withdrawIndex_ == TOTAL_SUPPLY) { //Reset index to start if it reaches the end of the array.
+                withdrawIndex_ = 0;
+            }
         }
-    } 
 
-    /// @notice To use this fallback function, the user must first have approved this contract to spend their tokens.
-    /// @notice Users who want to buy NFTs from the wrapper can send in GWEI avax to the JOINT404 address equal to the number of NFTs they wish to purchase.
-    /// @notice It will deduct the amount of ERC20 from them to pay for it.
-    /// @notice Then they must send the total collection size plus 1 to withdraw the NFTs.
-    /// @notice To sell, they send the amount put a 1 before the NFT ID they wish to sell into tokens, which will get sent to them instantly.
+        withdrawIndex = withdrawIndex_;
+    }
+
+
+        /// @dev Transfers ERC721 tokens from the sender to the receiver.
+        /// @param sender_ The address of the sender.
+        /// @param receiver_ The address of the receiver.
+        /// @param tokenId_ The ID of the ERC721 token to transfer.
+    function nftTransfer(address sender_, address receiver_, uint256 tokenId_) private {
+        require(INFT.transferFrom(sender_, receiver_, tokenId_), "ERC721 token transfer unsuccessful");
+    }
+
+
+        /// @dev Transfers ERC20 tokens from the sender to the receiver.
+        /// @param sender_ The address of the sender.
+        /// @param receiver_ The address of the receiver.
+        /// @param amount_ The amount of ERC20 tokens to transfer.
+    function tokenTransfer(address sender_, address receiver_, uint256 amount_) private {
+        require(ITOKEN.transferFrom(sender_, receiver_, amount_), "ERC20 token transfer unsuccessful");
+    }
+
+
+
 }
 
+
+
+
+   //Launch instructions for new LP of existing ERC721:
+
+//Launch the ERC20 token first so we have it's address, then use that address to launch this contract (or change to immutable and set in constructor with interfaces).
+
+//Transfer all ERC20 token supply to this contract via the ERC20 contract transferFrom function directly. 
+//(Do not use this contract's depositERC20 function to seed the initial ERC20 tokens).
+//(The token holder will need to have the unrestricted ability to transfer all the ERC20 tokens to this contract before the IDO launch.)
+
+//Next, deposit NFTS by calling this contract's depositERC721Array() to withdraw ERC20 tokens. Repeat until you have enough ERC20 tokens to LP with.
+//(Do not send ERC721 NFTs directly to this address via the ERC20 contract transferFrom function.)
+//(This JOINT404 contract will need to have the unrestricted ability to transfer all the ERC20 tokens to at least the LP seeder before the IDO launch.)
+
+//Pair the ERC20 tokens withdrawn with AVAX on a DEX, presumably at the same value as their recently minted price.
+//(The LP seeder will need to have the unrestricted ability to transfer all the ERC20 tokens to the DEX LP contract before the IDO launch.)
+
+//Release DAPP for this contract.
+//Launch the ERC20 IDO.
+
+//*If pre-IDO transfer restrictions, they should be removed for this contract and the ERC20 token supply minter.
+//*If token taxes, consider removing them for transfers involving this contract, (alternatively only tax this contract's transfers).
+//*If weird amount needed with taxes then dapp could calculate it for the user based on number of NFTs to "buy". Dapp can pre-validate the user's balances and approvals.
+
+//*To "burn" excess NFT mint supply but keep all the NFT IDs in circulation, we call depositERC721Array() here and then transfer the received ERC20 tokens to 0xdEaD. 
+//(Only works for FIFO array method here or a random shuffle version, would not work for a FILO version of this contract)
